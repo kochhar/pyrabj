cdb29816803fd9aab80d32a9e4c9aa0ac3e64ea4
Shailesh Kochhar <kochhar@metaweb.com>
2012-02-15 15:07:21 -0800

    Deleted a simple test

--- tests/simpletest.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import rabj.simple as s
-srv = s.RabjServer(s.RABJ_PROD)
-pqs = srv.queues_by_tags(['matchmaker2'], access_key='rabjrootaccesskey_09302009')
-myq = pqs[3]
-qques = myq.get_one()
-print qques.get_state(myq['id'])
-print qques.judgments()
